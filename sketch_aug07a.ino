c849cfdda8f6223089e80d8fd9440c08cf6adc1a
Calvin Houser <49534966+xeroxcat@users.noreply.github.com>
2019-10-22 18:02:47 -0700

    added routines for multiplexed button events
    1/15/18

--- sketch_aug07a.ino
+++ sketch_aug07a.ino
@@ -1,3 +1,6 @@
+#include <TM1637Display.h>
+
+
 //#include <ADSR.h>
 //#include <AudioConfigHiSpeed14bitPwm.h>
 //#include <AudioConfigStandard9bitPwm.h>
@@ -52,198 +55,435 @@
 #include <tables/saw2048_int8.h>
 #include <tables/triangle2048_int8.h>
 #include <tables/square_no_alias_2048_int8.h>
-#include <tables/logistic2048_int8.h>
+//#include <tables/logistic2048_int8.h>
 #include <tables/cyc2048_int8.h>
-
 
 #include <PS2KeyAdvanced.h>
 #include <PS2KeyCode.h>
 #include <PS2KeyTable.h>
-
+#include "NoteEnvelope.h"
 
 #define DATAPIN 4
 #define IRQPIN  3
-#define DIGIN1 5
-#define DIGIN2 6
-#define DIGIN3 7
-#define DIGIN4 8
-#define DIGINA 10
-#define DIGINB 11
-#define DIGINC 12
-#define DIGIND 13
 #define AUDIOUT 9
 #define POT1 0
+#define POT2 1
+#define POT3 2
+#define POT4 3
+#define POT5 4
+#define POT6 5
+#define BUTT_A 0
+#define BUTT_B 1
+#define BUTT_C 2
+#define BUTT_D 3
+#define BUTT_E 4
+#define BUTT_F 5
+#define BUTT_G 6
+#define BUTT_H 7
+#define BUTT_I 8
+#define BUTT_J 9
+#define BUTT_K 10
+#define BUTT_L 11
+#define BUTT_M 12
+#define BUTT_N 13
+#define BUTT_O 14
+#define BUTT_P 15
+#define BUTT_NONE -1
+#define CLK 1
+#define DIO 2
 
-Oscil <2048, AUDIO_RATE> osc1[6] = {
-  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA),
-  Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA), Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA),
-  Oscil <2048, AUDIO_RATE>(LOGISTIC2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
-};
-Oscil <2048, AUDIO_RATE> osc2[6] = {
-  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA),
-  Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA), Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA),
-  Oscil <2048, AUDIO_RATE>(LOGISTIC2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
-};
-Oscil <2048, AUDIO_RATE> osc3[6] = {
-  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA),
-  Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA), Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA),
-  Oscil <2048, AUDIO_RATE>(LOGISTIC2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
-};
-Oscil <2048, AUDIO_RATE> osc4[6] = {
-  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA),
-  Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA), Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA),
-  Oscil <2048, AUDIO_RATE>(LOGISTIC2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
-};
-Oscil <2048, AUDIO_RATE> osc5[6] = {
-  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA),
-  Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA), Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA),
-  Oscil <2048, AUDIO_RATE>(LOGISTIC2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
+struct Note {
+  float noteFreq = 0;
+  NoteEnvelope atkDec = new NoteEnvelope(CONTROL_RATE);
 };
 
-float oneFreq = 0;
-float twoFreq = 0;
-float thrFreq = 0;
-float fouFreq = 0;
-float fivFreq = 0;
+Oscil <2048, AUDIO_RATE> osc1[] = {
+  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA), Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA),
+  Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
+};
+Oscil <2048, AUDIO_RATE> osc2[] = {
+  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA), Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA),
+  Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
+};
+Oscil <2048, AUDIO_RATE> osc3[] = {
+  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA), Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA),
+  Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
+};
+Oscil <2048, AUDIO_RATE> osc4[] = {
+  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA), Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA),
+  Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
+};
+Oscil <2048, AUDIO_RATE> osc5[] = {
+  Oscil <2048, AUDIO_RATE>(SIN2048_DATA), Oscil  <2048, AUDIO_RATE>(SAW2048_DATA), Oscil  <2048, AUDIO_RATE>(TRIANGLE2048_DATA),
+  Oscil  <2048, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA), Oscil <2048, AUDIO_RATE>(CYC2048_DATA)
+};
 
-int digIn1 = 0;
-int buttonTiming = 0;
-int oscTog = 0;
+//int8_t* waveTables[] = {"SIN2048_DATA","SAW2048_DATA","TRIANGLE2048_DATA","SQUARE_NO_ALIAS_2048_DATA","CYC2048_DATA"};
+//float oneFreq = 0;
+//float twoFreq = 0;
+//float thrFreq = 0;
+//float fouFreq = 0;
+//float fivFreq = 0;
+
+//int buttonTiming = 0;
+  //Cycle Bounds:4,0,1,1,1, 0,0,1,1,1, 3,1,1, 3,1,1
+  //Button Label:A,B,C,D,E, F,G,H,I,J, K,L,M, N,O,P
+byte buttons[] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0, 0,0,0};
+byte activeButton = BUTT_NONE;//last button pressed
+byte pollTimer = 0;
+byte activePoll = 0;//countdown timer 64-0 (1seconds) till last button can be pressed again
+byte noisePoll = 0;
+Note noteOne;
+Note noteTwo;
+Note noteThree;
+Note noteFour;
+Note noteFive;
 
 uint16_t c;
 PS2KeyAdvanced keyboard;
+TM1637Display display(CLK, DIO);
 
 
 void setup()
 {
   startMozzi(CONTROL_RATE);
   //osc.setFreq(one_freq);
+  //noteOne.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+  //noteTwo.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+  //noteThree.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+  //noteFour.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+  //noteFive.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
   pinMode(AUDIOUT, OUTPUT);
-  pinMode(DIGIN1, INPUT);
-  pinMode(DIGIN2, INPUT);
-  pinMode(DIGIN3, INPUT);
-  pinMode(DIGIN4, INPUT);
-  pinMode(DIGINA, INPUT);
-  pinMode(DIGINB, INPUT);
-  pinMode(DIGINC, INPUT);
-  pinMode(DIGIND, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+  pinMode(13, INPUT);
   keyboard.begin( DATAPIN, IRQPIN ); // Configure the keyboard library
-  Serial.begin( 115200 );
+  //display.
+  //Serial.begin(9600);
+  display.setBrightness(0x0a);
+  display.showNumberDec(1,true);
+  /*
   #if defined(ARDUINO_ARCH_AVR)
     Serial.println( F( "PS2 Advanced Key Simple Test:" ) );
   #elif defined(ARDUINO_ARCH_SAM)
     Serial.println( "PS2 Advanced Key Simple Test:" );
   #endif
+  */
 }
 
 void updateControl(){
-  osc1[oscTog].setFreq(oneFreq);
-  osc2[oscTog].setFreq(twoFreq);
-  osc3[oscTog].setFreq(thrFreq);
-  osc4[oscTog].setFreq(fouFreq);
-  osc5[oscTog].setFreq(fivFreq);
+  osc1[buttons[BUTT_A]].setFreq(noteOne.noteFreq);
+  osc2[buttons[BUTT_A]].setFreq(noteTwo.noteFreq);
+  osc3[buttons[BUTT_A]].setFreq(noteThree.noteFreq);
+  osc4[buttons[BUTT_A]].setFreq(noteFour.noteFreq);
+  osc5[buttons[BUTT_A]].setFreq(noteFive.noteFreq);
+  noteOne.atkDec.nextVal();
+  noteTwo.atkDec.nextVal();
+  noteThree.atkDec.nextVal();
+  noteFour.atkDec.nextVal();
+  noteFive.atkDec.nextVal();
   
-  if( keyboard.available() ) {
+  if( keyboard.available() ) {//keyb read function
   c = keyboard.read(); // read the next key
+
   if( c > 0 ) {
-      int note_val = note(c);
+      int note_val = note(c);//note_val: 4 digits, 0xxx vs 1xxx --> key is on or off (key == xxx)
+      //Serial.print(" ");
+      //Serial.print(note_val);
       if(note_val < 1000) {
-        if (findFreq(note_val) == oneFreq 
-        || findFreq(note_val) == twoFreq 
-        || findFreq(note_val) == thrFreq 
-        || findFreq(note_val) == fouFreq
-        || findFreq(note_val) == fivFreq){
-        } else if(oneFreq==0){
-          oneFreq = findFreq(note_val);
-        } else if(twoFreq==0){
-          twoFreq = findFreq(note_val);
-        } else if(thrFreq==0){
-          thrFreq = findFreq(note_val);
-        } else if(fouFreq==0){
-          fouFreq = findFreq(note_val);
-        } else if(fivFreq==0){
-          fivFreq = findFreq(note_val);
+        if (findFreq(note_val) == noteOne.noteFreq 
+        || findFreq(note_val) == noteTwo.noteFreq 
+        || findFreq(note_val) == noteThree.noteFreq 
+        || findFreq(note_val) == noteFour.noteFreq
+        || findFreq(note_val) == noteFive.noteFreq){
+
+        } else if(noteOne.noteFreq==0){//initialize a new key press
+          noteOne.noteFreq = findFreq(note_val);
+          noteOne.atkDec.instantiateEnv(buttons[BUTT_K],buttons[BUTT_N]);
+        } else if(noteTwo.noteFreq==0){
+          noteTwo.noteFreq = findFreq(note_val);
+          noteTwo.atkDec.instantiateEnv(buttons[BUTT_K],buttons[BUTT_N]);
+        } else if(noteThree.noteFreq==0){
+          noteThree.noteFreq = findFreq(note_val);
+          noteThree.atkDec.instantiateEnv(buttons[BUTT_K],buttons[BUTT_N]);
+        } else if(noteFour.noteFreq==0){
+          noteFour.noteFreq = findFreq(note_val);
+          noteFour.atkDec.instantiateEnv(buttons[BUTT_K],buttons[BUTT_N]);
+        } else if(noteFive.noteFreq==0){
+          noteFive.noteFreq = findFreq(note_val);
+          noteFive.atkDec.instantiateEnv(buttons[BUTT_K],buttons[BUTT_N]);
         }
-      } if(findFreq(note_val - 1000)==oneFreq){
-        oneFreq = 0;
-      } if(findFreq(note_val - 1000)==twoFreq){
-        twoFreq = 0;
-      } if(findFreq(note_val - 1000)==thrFreq){
-        thrFreq = 0;
-      } if(findFreq(note_val - 1000)==fouFreq){
-        fouFreq = 0;
-      } if(findFreq(note_val - 1000)==fivFreq){
-        fivFreq = 0;
+      } if(findFreq(note_val - 1000)==noteOne.noteFreq){//finish a key press
+        noteOne.noteFreq = 0;
+      } if(findFreq(note_val - 1000)==noteTwo.noteFreq){
+        noteTwo.noteFreq = 0;
+      } if(findFreq(note_val - 1000)==noteThree.noteFreq){
+        noteThree.noteFreq = 0;
+      } if(findFreq(note_val - 1000)==noteFour.noteFreq){
+        noteFour.noteFreq = 0;
+      } if(findFreq(note_val - 1000)==noteFive.noteFreq){
+        noteFive.noteFreq = 0;
       }
     }
   }
-  digIn1 = buttonVal(DIGIN1,digIn1);
-  if (digIn1 == 1){
-    oscTogCounter();
-  }
+  buttonsManager(buttonVal());
   /*
   Serial.print("\n");
-  Serial.print(oneFreq);
+  Serial.print(pollTimer);
+  Serial.print("-");
+  Serial.print(noisePoll);
+  Serial.print("-");
+  Serial.print(activePoll);
+  Serial.print("-");
+  Serial.print(buttonVal());
+  
+  
+  for(int i=0;i<=15;i++){
+    if(buttons[i]!=0){
+      int j = buttons[i];
+      Serial.print(" ");
+      Serial.print(i);
+      Serial.print("-");
+      Serial.print(j);
+      Serial.print(" ");
+      //display.showNumberDec(i);
+    }
+  }
+  
+  //Serial.print(oneFreq);
+  
   Serial.print(" ");
-  Serial.print(twoFreq);
-  Serial.print(" ");
-  Serial.print(thrFreq);
-  Serial.print(" ");
-  Serial.print(fouFreq);*/
+  Serial.print(noteOne.atkDec.currentVal());
+  //Serial.print(" ");
+  //Serial.print(fouFreq);
+  //Serial.print(" ");
+  //Serial.print(fivFreq);
+  
   Serial.print("\n");
-  Serial.print(mozziAnalogRead(POT1));
-  Serial.print(" ");
-  Serial.print(oscTog);
-  Serial.print(" ");
-  Serial.print(digIn1);
-  Serial.print(" ");
-  Serial.print(digitalRead(DIGIN1));
-  Serial.print(digitalRead(DIGIN2));
-  Serial.print(digitalRead(DIGIN3));
-  Serial.print(digitalRead(DIGIN4));
-  Serial.print(digitalRead(DIGINA));
-  Serial.print(digitalRead(DIGINB));
-  Serial.print(digitalRead(DIGINC));
-  Serial.print(digitalRead(DIGIND));
+  */
+  //Serial.print(mozziAnalogRead(POT1));
+  //display.showNumberDec(POT1,true);
 }
 
 int updateAudio(){
-  return ((osc1[oscTog].next()+osc2[oscTog].next()+osc3[oscTog].next()+osc4[oscTog].next()+osc5[oscTog].next())>>2);
-  //return (osc1[oscTog].next());
+    int note1 = osc1[buttons[BUTT_A]].next();
+    //int envl1 = 
+    int note2 = osc2[buttons[BUTT_A]].next();
+    int note3 = osc3[buttons[BUTT_A]].next();
+    int note4 = osc4[buttons[BUTT_A]].next();
+    int note5 = osc5[buttons[BUTT_A]].next();
+    return note1;
+  //return ((note1+note2+note3+note4+note5)>>2);
+  //adds each of the oscillator values, returns the result, bitshifted twice to the right
 }
 
 void loop(){
 audioHook();
 }
-
-
-
-int buttonVal(int pin, int var) {//returns state of button on specified digital pin
-  boolean state = digitalRead(pin);//currently pressed or not
-  
-  if (!state && (var == 0) && (buttonTiming == 0)) {
-    return 0;//unpressed
-  } else if (state && (var == 2)) {
-    return 2;//pressed
-  } else if (!state && (var == 3) && (buttonTiming == 0)) {
-    return 0;//unpressed
-  } else if (!state && (var == 3)) {
-    buttonTimer();
-    return 3;//unpressed
-  } else if (state && (var == 1)) {
-    return 2;//pressed
-  } else if (state && (var == 3)) {
-    return 2;//pressed
-  } else if (!state && (var == 2)) {
-    buttonTimer();
-    return 3;//release
-  } else if (state && (var == 0)) {
-    return 1;//press register
-  } else {
-    return 0;
+/*
+char buttonRead(char buttonId){//input: A-P, output: cycle position of input
+  char buttonIndex = buttonId-65;
+  if (buttonId < 65){
+    return -1;
   }
+  return buttons[buttonIndex];
+}
+*/
+byte buttonVal() {//returns any valid button press 0-15, -1 if multiple/none
+  char state = 0;
+  for (int i=5;i<=8;i++){
+    if (digitalRead(i)&&state==0){
+      state = i-4;
+      for (int j=10; j<=13; j++){
+        if (digitalRead(j)&&state<=4){
+          state = state+4*(j-9);
+        } else if (digitalRead(j)&&state>4){
+          return BUTT_NONE;
+        }
+      }
+    }else if (digitalRead(i)&&state>0){
+      return BUTT_NONE;
+    } 
+  }
+  switch (state){
+    case 0:
+      return BUTT_NONE;
+    case 5:
+      return BUTT_E;
+    case 6:
+      return BUTT_D;
+    case 7:
+      return BUTT_B;
+    case 8:
+      return BUTT_C;
+    case 9:
+      return BUTT_J;
+    case 10:
+      return BUTT_I;
+    case 11:
+      return BUTT_G;
+    case 12:
+      return BUTT_H;
+    case 13:
+      return BUTT_M;
+    case 14:
+      return BUTT_L;
+    case 15:
+      return BUTT_P;
+    case 16:
+      return BUTT_K;
+    case 17:
+      return BUTT_A;
+    case 18:
+      return BUTT_F;
+    case 19:
+      return BUTT_N;
+    case 20:
+      return BUTT_O;
+  }
+  
 }
 
+void buttonsManager(byte pressed){
+  if (pollTimer == 0) {//do nothing
+    if (pressed == BUTT_NONE) {//release or multiple presses
+    } else {
+      activeButton = pressed;
+      pollTimer = 64;
+    }
+    return;
+  } if (pollTimer > 1) {
+      if (pressed == BUTT_NONE) {
+        pollTimer--;
+      } else if (activeButton == pressed) {
+        activePoll++;
+        pollTimer--;
+      } else {
+        noisePoll++;
+        pollTimer--;
+      }
+  } else {
+    //noteOne.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+    //noteTwo.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+    //noteThree.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+    //noteFour.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+    //noteFive.noteFreq.setTable(waveTables[buttons[BUTT_A]]);
+    if (activePoll/noisePoll > 0) {
+      switch (activeButton){//cycling actions
+        case BUTT_A://osctog
+          if (buttons[BUTT_A] < 4){
+            buttons[BUTT_A]++;
+          } else {
+            buttons[BUTT_A]=0;
+          }
+          break;
+        case BUTT_B:
+          
+          break;
+        case BUTT_C://hipass invert attack/decay
+          if (buttons[BUTT_C] == 1) {
+            buttons[BUTT_C] = 0;
+          } else {
+            buttons[BUTT_C] = 1;
+          }
+          break;
+        case BUTT_D://effect 1 attack toggle
+          if (buttons[BUTT_D] == 1) {
+            buttons[BUTT_D] = 0;
+          } else {
+            buttons[BUTT_D] = 1;
+          }
+          break;
+        case BUTT_E://effect 2 attack toggle
+          if (buttons[BUTT_E] == 1) {
+            buttons[BUTT_E] = 0;
+          } else {
+            buttons[BUTT_E] = 1;
+          }
+          break;
+        case BUTT_F:
+          
+          break;
+        case BUTT_G:
+          
+          break;
+        case BUTT_H://lopass invert attack/decay
+          if (buttons[BUTT_H] == 1) {
+            buttons[BUTT_H] = 0;
+          } else {
+            buttons[BUTT_H] = 1;
+          }
+          break;
+        case BUTT_I://effect 1 decay toggle
+          if (buttons[BUTT_I] == 1) {
+            buttons[BUTT_I] = 0;
+          } else {
+            buttons[BUTT_I] = 1;
+          }
+          break;
+        case BUTT_J://effect 2 decay toggle
+          if (buttons[BUTT_J] == 1) {
+            buttons[BUTT_J] = 0;
+          } else {
+            buttons[BUTT_J] = 1;
+          }
+          break;
+        case BUTT_K://attack function cycle
+          if (buttons[BUTT_K] < 3){
+            buttons[BUTT_K]++;
+          } else {
+            buttons[BUTT_K]=0;
+          }
+          break;
+        case BUTT_L://hipass attack toggle
+          if (buttons[BUTT_L] == 1) {
+            buttons[BUTT_L] = 0;
+          } else {
+            buttons[BUTT_L] = 1;
+          }
+          break;
+        case BUTT_M://lopass attack toggle
+          if (buttons[BUTT_M] == 1) {
+            buttons[BUTT_M] = 0;
+          } else {
+            buttons[BUTT_M] = 1;
+          }
+          break;
+        case BUTT_N://decay function cycle
+          if (buttons[BUTT_N] < 3){
+            buttons[BUTT_N]++;
+          } else {
+            buttons[BUTT_N]=0;
+          }
+          break;
+        case BUTT_O://hipass decay toggle
+          if (buttons[BUTT_O] == 1) {
+            buttons[BUTT_O] = 0;
+          } else {
+            buttons[BUTT_O] = 1;
+          }
+          break;
+        case BUTT_P://lopass decay toggle
+          if (buttons[BUTT_P] == 1) {
+            buttons[BUTT_P] = 0;
+          } else {
+            buttons[BUTT_P] = 1;
+          }
+          break;
+      }
+    }
+    pollTimer = 0;
+    noisePoll = 0;
+    activePoll = 0;
+    activeButton = BUTT_NONE;
+  }
+}
+/*
 void buttonTimer(){
   if (buttonTiming == 31) {
     buttonTiming = 0;
@@ -251,15 +491,16 @@ void buttonTimer(){
     buttonTiming++; 
   }
 }
-
-void oscTogCounter() {
-  if (oscTog == 5) {
-    oscTog = 0;
+*/
+/*
+void buttonRead('A')Counter() {
+  if (buttonRead('A') == 5) {
+    buttonRead('A') = 0;
   } else {
-    oscTog++;
+    buttonRead('A')++;
   }
 }
-
+*/
 int note(uint16_t val) {
   int noteCode = 0;
   if(val > 999) {
@@ -341,6 +582,7 @@ int note(uint16_t val) {
     case 0x1D:
       return (noteCode+410);//C4
   }
+  //return notecode?
 }
 
 float findFreq(int note_val){
